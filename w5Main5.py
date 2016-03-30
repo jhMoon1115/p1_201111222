cf39dc033b22a420117a984275fbbde379c7d79b
jhMoon <answjdgus222@gmail.com>
2016-03-30 15:35:50 +0900

    0330 수업시간 과제 제출
    성적 처리 함수 과제

---
+++ w5Main5.py
@@ -0,0 +1,26 @@
+﻿def computeGrade(marks) :
+	if marks<=100 and marks>=90 :
+		return "A"
+	elif 80<=marks<90 :
+		return "B"
+	elif 60<=marks<80 :
+		return "C"
+	elif 40<=marks<60 :
+		return "D"
+	elif 0<=marks<40 :
+		return "F"
+	else :
+		return "Marks have to be number between 0 and 100"
+
+def lab1() :
+	marks=float(raw_input("enter marks : "))
+	grade=computeGrade(marks)
+	print grade
+
+
+def main() :
+	lab1()
+	raw_input("press 'any key' and 'enter key' if you want to finish...")
+
+
+main()
