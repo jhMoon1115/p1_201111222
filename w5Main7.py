2e606a53e18b8c9cd86d2fd863282fbd822f9692
jhMoon <answjdgus222@gmail.com>
2016-04-04 13:53:34 +0900

    0404 삼각형 과제 제출
    삼각형 그리기

---
+++ w5Main7.py
@@ -0,0 +1,14 @@
+﻿def drawTriangle(char, len) :
+	for i in range(1, len+1) :
+		print ' '*(len-i)+'*'*(2*i-1)
+
+def lab7() :
+	char='*'
+	len=10
+	drawTriangle(char, len)
+
+def main() :
+	lab7()
+	raw_input("\npress 'any key' and 'enter key' if you want to finish...")
+
+main()
