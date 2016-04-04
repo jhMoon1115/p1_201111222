ba9fa78522de49c4f2bb9419c00c8b5a6dce2b7d
jhMoon <answjdgus222@gmail.com>
2016-04-04 15:11:15 +0900

    0404 삼각형 그리기 및 BMI 통합 code
    삼각형 그리기 까지 통합하여 업로드

---
+++ w5Main.py
@@ -0,0 +1,20 @@
+﻿"""
+@author mjh
+@since 160404
+"""
+
+def drawTriangle(char, len) :
+	for i in range(1, len+1) :
+		print ' '*(len-i)+'*'*(2*i-1)
+
+
+def lab5() :
+	char='*'
+	len=10
+	drawTriangle(char, len)
+
+def main() :
+	lab5()
+
+if __name__=="__main__"
+	main()
