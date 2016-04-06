e6db0ce3c71d23622f4de73f0310366c75f7ff15
jhMoon <answjdgus222@gmail.com>
2016-04-06 14:36:46 +0900

    0406 3과 5의 배수 더하기
    3과 5의 배수 더하기

---
+++ w6Main.py
@@ -0,0 +1,26 @@
+﻿def sumMultiplesOf3And5() :
+	sum=0
+	for i in range(1, 1001) :
+		if(i%3==0 or i%5==0) :
+			sum+=i
+	return sum
+
+
+def isLeapYear(year) :
+	if (year%4==0) and (year%100!=0 or year%400==0) :
+		return True
+	return False
+
+
+
+def lab6() :
+	sum=sumMultiplesOf3And5()
+	print "Main 9, Sum of Multiples of 3 and 5 : {0:d}".format(sum)
+
+def main() :
+	lab6()
+
+if __name__=="__main__" :
+	main()
+
+	
