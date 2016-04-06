77fd9bfb65188156601adca3f17ca12feb92d344
jhMoon <answjdgus222@gmail.com>
2016-04-06 14:53:14 +0900

    0406 3과 5의 배수 더하기

--- w6Main.py
+++ w6Main.py
@@ -3,14 +3,17 @@
 @since 0406
 """
 
-def sumMultiplesOf3And5() :
+def sumMultiplesOf3And5(begin, end) :
 	"""
 	Main 9
 	sum Multiples of 3 and 5 function
 
 	parameters
 	----------
-	none
+	arg1 : int
+		begin
+	arg2 : int
+		end
 
 	Returns
 	-------
@@ -21,7 +24,7 @@ def sumMultiplesOf3And5() :
 	sumMultiplesOf3And5()
 	"""
 	sum=0
-	for i in range(1, 1001) :
+	for i in range(begin, end+1) :
 		if(i%3==0 or i%5==0) :
 			sum+=i
 	return sum
@@ -29,7 +32,7 @@ def sumMultiplesOf3And5() :
 
 
 def lab6() :
-	sum=sumMultiplesOf3And5()
+	sum=sumMultiplesOf3And5(1, 1000)
 	print "Main 9, Sum of Multiples of 3 and 5 : {0:d}".format(sum)
 
 def main() :
