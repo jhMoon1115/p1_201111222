0fb908d96cdbd1b018c57c0d111819531e16b682
jhMoon <answjdgus222@gmail.com>
2016-04-18 14:08:00 +0900

    0418 data structure main 3 까지
    거북이 사각형 경로 저장, 포인트로 사각형 그리기

--- w7Main.py
+++ w7Main.py
@@ -28,17 +28,45 @@ def drawSquareAtSave(pos, size) :
 	return tracks
 
 
-def lab7() :
+def lab7_2() :
 	"""
 	0418
 	"""
 	tracks=drawSquareAtSave((100, 100), 100)
 	print "Tracks of Square : ",
 	print tracks
-	wn.exitonclick()
+
+def drawSquareFrom(tracks) :
+	"""
+	draw square from tracks function
+	
+	parameter
+	---------
+	arg1 list
+		tracks
+	return
+	------
+	none
+	"""
+	for i in range(0, len(tracks)) :
+		if(i==0) :
+			t.penup()
+			t.setpos(tracks[i])
+			t.pendown()
+		else :
+			t.setpos(tracks[i])
+	t.setpos(tracks[0])
+
+def lab7_3() :
+	tracks=[(0, 0), (100, 0), (100, -150), (0, -150)]
+	print "\ndraw square from tracks - ",
+	print tracks
+	drawSquareFrom(tracks)
 
 def main() :
-	lab7()
+	lab7_2()
+	lab7_3()
+	wn.exitonclick()
 
 if(__name__=="__main__") :
 	main()
