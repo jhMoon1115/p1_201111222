9b93dc149a8904e3ce0bd4b956cf2f1f462bfa61
jhMoon <answjdgus222@gmail.com>
2016-04-20 15:40:39 +0900

    0420 (데이터 구조4) 추가
    7주차 거북이가 걸어간 트랙을 다시 걷게 하기 추가

--- w7Main.py
+++ w7Main.py
@@ -63,9 +63,63 @@ def lab7_3() :
 	print tracks
 	drawSquareFrom(tracks)
 
+def saveTracks() :
+	tracks=[t.pos()]
+	t.right(180)
+	t.forward(200)
+	tracks.append(t.pos())
+	t.right(90)
+	t.forward(100)
+	tracks.append(t.pos())
+	t.left(90)
+	t.forward(400)
+	tracks.append(t.pos())
+	t.right(90)
+	t.forward(100)
+	tracks.append(t.pos())
+	t.right(90)
+	t.forward(200)
+	tracks.append(t.pos())
+	t.left(90)
+	t.forward(150)
+	tracks.append(t.pos())
+	t.left(90)
+	t.forward(250)
+	tracks.append(t.pos())
+	t.right(90)
+	t.forward(90)
+	tracks.append(t.pos())
+	return tracks
+
+def replayTracks(tracks) :
+	t.reset()
+	t.setpos(tracks[0])
+	t.clear()
+	t.speed(1)
+	t.pencolor("Blue")
+	for i in range(1,len(tracks)) :
+		t.setpos(tracks[i])
+
+def lab7_4() :
+	"""
+	0420
+	Main 4-거북이가 걸어간 트랙을 다시 걷게 하기
+	"""
+	wn.bgpic("maze.gif")
+	t.reset()
+	t.clear()
+	t.speed(1)
+	t.shape("turtle")
+	t.pencolor("Red")
+	t.goto(380, -200)
+	t.clear()
+	tracks=saveTracks()
+	replayTracks(tracks)
+
 def main() :
 	lab7_2()
 	lab7_3()
+	lab7_4()
 	wn.exitonclick()
 
 if(__name__=="__main__") :
