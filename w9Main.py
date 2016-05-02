1e5d844dd420e838e672a5c20d71eb8487311040
jhMoon <answjdgus222@gmail.com>
2016-05-02 15:01:30 +0900

    0502 9주차 과제
    5. 문자열에서 문자세기
    6. 입력받은 문자열에서 숫자와 문자 갯수 세기 및 그래프 그리기
    7. 우리집에는 없지만 친구집에 있는 가전제품 찾가

---
+++ w9Main.py
@@ -0,0 +1,79 @@
+﻿import matplotlib
+import matplotlib.pyplot as plt
+
+def charCount(word) :
+	d=dict()
+	for i in word :
+		#if not d.get(i) :
+			#d.update({i : 1})
+		if i not in d :
+			d[i]=1
+		else : 
+			d[i]+=1
+	return d
+
+def countDigitsLetters(word) :
+	d=dict({"digit" :0, "char" :0})
+	for c in word :
+		if c.isdigit() :
+			d["digit"]+=1
+		else :
+			d["char"]+=1
+	return d
+
+def lab9_5() :
+	print "===Main 5==="
+	word="sangmyung university"
+	print charCount(word)
+
+def lab9_6() :
+	word="sangmyung university, 20, hongjimun 2gil, jongnogu, seoul, 03016, republic of korea"
+	d=countDigitsLetters(word)
+	print "\n===Main 6==="
+	print d
+	plt.bar(range(len(d)), d.values())
+	plt.xticks(range(len(d)), list(d.keys()))
+	plt.show()
+
+def lab9_7() :
+	print "\n===Main 7==="
+	mine={"TV", "phone", "camera", "fridger", "mixer", "audio", "cd player", "light", "computer", "notebook", "recoder"}
+	friend={"coffee machine", "radio", "camera", "running machine", "ramp", "computer", "notebook", "recoder"}
+
+	all=mine.union(friend) #모든 가전제품
+	onlyMine=all.difference(mine) #내 방에만 있는 가전제품
+	print "** In only my room : ",
+	print onlyMine
+
+	onlyFriend=all.difference(friend) #친구 방에만 있는 가전제품
+	print "** In only friend's room : ",
+	print onlyFriend
+
+	print "** In all room : ",
+	common=set() 
+	for i in mine :
+		if i in friend :
+			common.add(i)# 공통된 가전제품
+	print common
+
+	print "** All electronic machine : "
+	print all
+	
+	print "** Count electronic machine"
+	for i in all :
+		if i in common :
+			print i,
+			print 2
+		else :
+			print i,
+			print 1
+
+
+			
+def main() :
+	lab9_5()
+	lab9_6()
+	lab9_7()
+
+if __name__=="__main__" :
+	main()
