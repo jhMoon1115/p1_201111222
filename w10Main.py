8a01bef393f543a16a8371cb18b050bed288edbc
201111222_문정현 <answjdgus222@gmail.com>
2016-05-04 14:56:23 +0900

    0504 lab8
    경복궁과 가장 가까운 역 찾기

---
+++ w10Main.py
@@ -0,0 +1,27 @@
+﻿#시청역 37.565738    126.976851
+#서대문역 37.565761   126.966639
+#종로3가역  37.570414  126.992129
+#을지로입구역 37.566064 126.982608
+#광화문역  37.571610  126.976436
+#경복궁역 37.575829   126.973516
+
+import math
+
+
+def lab10_8() : 
+	locations=[(37.565738, 126.976851), (37.565761, 126.966639), (37.570414, 126.992129), (37.566064, 126.982608), (37.571610 ,126.976436)]
+	kyungBok=(37.575829, 126.973516)
+
+	dis=list()
+
+	for loc in locations :
+		dis.append(math.sqrt((loc[0]-kyungBok[0])**2 + (loc[1]-kyungBok[1])**2))
+
+	print "distance of kyungBok station from nearest station : ",min(dis)
+
+def main() :
+	lab10_8()
+
+
+if __name__=="__main__" :
+	main()
