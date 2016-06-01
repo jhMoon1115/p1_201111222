22be3154101a0c55ab0ac8c0fc461fafcd758da5
201111222_문정현 <answjdgus222@gmail.com>
2016-06-01 15:45:21 +0900

    0601 14주차 과제 제출
    개를 짖게 하기

---
+++ w14Main.py
@@ -0,0 +1,34 @@
+﻿class Dog(object) : 
+	def __init__(self, name) :
+		self.name=name
+	def getName(self) :
+		print "my dog name is {0}".format(self.name)
+	def talk(self) :
+		print "mung mung..."
+
+class Shitzu(Dog) :
+	def talk(self) :
+		print "si si!!"
+
+class Maltese(Dog) :
+	def talk(self) :
+		print "mal mal!!"
+		
+
+def lab14_1() :
+	print "Dog talk .."
+	mydog=Dog('choco')
+	mydog.talk()
+	print "Shitzu talk .."
+	s=Shitzu('shi')
+	s.talk()
+	print "Martis talk .."
+	m=Maltese('malt')
+	m.talk()
+
+def main() :
+	lab14_1()
+	
+if __name__=='__main__' :
+	main()
+
